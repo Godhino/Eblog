168925b209257a41d9a53d6c1b42c725290b38fa
Godhino <gsautomobile4@gmail.com>
2025-06-03 02:18:26 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+console.log('Hello World!');
+
+function fun1(){
+  // Tab to edit
+  document.getElementById ('content').innerHTML= `
+  
+  <style>
+body {
+  background-color: black;
+}
+</style>
+<details>
+  <summary>about this website</summary>
+  <p>this is a Bloggers website you can view videos here and follow me on various social media platform like Facebook, TikTok,</p>
+  <i class="fab fa-facebook"></i>
+    <i class="fab fa-youtube"></i>
+      <i class="fab fa-tiktok"></i>
+</details>
+
+<h2>
+  <!--Tab to edit-->
+  welcome to the main page
+</h2>
+
+
+  
+  `;
+  
+  
+}
+
+function fun2() {
+  document.getElementById ('content').innerHTML= `
+  <style>
+  
+</style>
+<h2>
+  <!--Tab to edit-->
+  welcome to the search page
+</h2>
+
+<input type="search"><button>
+  <!--Tab to edit--><i class="fa fa-search">
+  <!--Tab to edit-->
+</i>
+</button>
+  
+  `;
+  
+  
+}
+
+function fun3() {
+  document.getElementById ('content').innerHTML= `
+  <style>
+  
+</style>
+<h2>
+  <!--Tab to edit-->
+  welcome to the search page
+</h2>
+
+<input type="search"><button>
+  <!--Tab to edit--><i class="fa fa-search">
+  <!--Tab to edit-->
+</i>
+</button>
+  
+  `;
+  
+  // Tab to edit
+}
+
+function fun4() {
+  // Tab to edit
+  document.getElementById ('content').innerHTML= `
+  <style>
+  
+</style>
+<h2>
+  <!--Tab to edit-->
+  welcome to the search page
+</h2>
+
+<input type="search"><button>
+  <!--Tab to edit--><i class="fa fa-search">
+  <!--Tab to edit-->
+</i>
+</button>
+  
+  `;
+  
+}
